3543b86b759ae11a7602e7660768565f57dda5d4
bharat patidar <patidarbharat095@gmail.com>
2021-03-08 14:34:53 +0530

    Correct the debug

--- final.c
+++ final.c
@@ -11,7 +11,7 @@ void reset_score();
 void help();
 void edit_score(float , char []);
  int asked(char option[size], const char option1[size], const char option2[size],
-	const char option3[size], const char option4[size],const char correct[size], const char category[size],FILE * fp,int *);
+	const char option3[size], const char option4[size],const char correct[size], const char category[size],FILE * fp,float *);
 	int main()
      {
     char text[size],option1[size], option2[size], option3[size], option4[size], option[size], correct[size], category[size];
@@ -38,19 +38,12 @@ void edit_score(float , char []);
 	}
   mainhome:
      system("cls");
+     printf("\n\t\t\tC PROGRAM QUIZ GAME\n");
      printf("\n\t\t_________________________________________");
-
-     printf("\n\t\t\t   WELCOME to THE GAME");
-     printf("\n\t   +-------+                          ___       ___                             ");
-     printf("\n\t   |       |      |         |            |                /                              ");
-     printf("\n\t   |       |      |         |            |               /                               ");
-     printf("\n\t   |       |      |         |            |              /                                ");
-     printf("\n\t   |    // |      |         |            |             /                                 ");
-     printf("\n\t   |   //  |      |         |            |            /                                  ");
-     printf("\n\t   +--//---+      |___|         _|_        /___                          ");
+     printf("\n\t\t\tWELCOME  to THE GAME  ");
      printf("\n\t\t________________________________________");
      printf("\n\t\t________________________________________");
-     printf("\n\t\t   BECOME A MILLIONAIRE!!!!!!!!!!!    ") ;
+     printf("\n\t\t GOOD CHANCE TO WIN CASH!!!!!!!!!!!    ") ;
      printf("\n\t\t________________________________________");
      printf("\n\t\t________________________________________");
      printf("\n\t\t > Press S to start the game");
@@ -73,7 +66,8 @@ void edit_score(float , char []);
 	}
 	else if (choice=='R')
 	{reset_score();
-	getch();
+	 printf("\nOld record was deleted\n");
+	 getch();
 	goto mainhome;}
 	else if (choice=='Q')
 	exit(1);
@@ -84,20 +78,13 @@ void edit_score(float , char []);
     printf("\n\n\n\n\n\n\n\n\n\n\t\t\tResister your name:");
      gets(playername);
      system("cls");
-    printf("\n ------------------  Welcome %s to C Program Quiz Game --------------------------",playername);
-    printf("\n\n Here are some tips you might wanna know before playing:");
-    printf("\n -------------------------------------------------------------------------");
-    printf("\n >> There are 2 rounds in this Quiz Game,WARMUP ROUND & CHALLANGE ROUND");
-    printf("\n >> In warmup round you will be asked a total of 3 questions to test your");
-    printf("\n    general knowledge. You are eligible to play the game if you give atleast 2");
-    printf("\n    right answers, otherwise you can't proceed further to the Challenge Round.");
-    printf("\n >> Your game starts with CHALLANGE ROUND. In this round you will be asked a");
-    printf("\n    total of 10 questions. Each right answer will be awarded $100,000!");
-    printf("\n    By this way you can win upto ONE MILLION cash prize!!!!!..........");
-    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
-    printf("\n    right option.");
-    printf("\n >> You will be asked questions continuously, till right answers are given");
-    printf("\n >> No negative marking for wrong answers!");
+    printf("\n -***...........Welcome %s to C Program Quiz Game ........***-",playername);
+    printf("\n\n ---------------------------------------------------------------------");
+    printf("\n >> Here are some tips you might wanna know before playing:");
+    printf("\n ---------------------------------------------------------------------\n");
+    printf("\n >> Now you are going to play WARMUP ROUND you will be asked a total of ");
+    printf("\n    5 questions");
+    printf("\n >> Give atleast 3 right answers otherwise you can't play the Game\n");
     printf("\n\n\t!!!!!!!!!!!!! ALL THE BEST !!!!!!!!!!!!!");
     printf("\n\n\n Press Y  to start the game!\n");
     printf("\n Press any other key to return to the main menu!");
@@ -145,16 +132,16 @@ void edit_score(float , char []);
 				}
 
 				if(i == 3)
-			strncpy(option1,line,30);
+			strncpy(option1,line,50);
 
 				if(i == 4)
-			strncpy(option2,line,30);
+			strncpy(option2,line,50);
 
 				if(i == 5)
-			strncpy(option3,line,30);
+			strncpy(option3,line,50);
 
 				if(i == 6)
-			strncpy(option4,line,30);
+			strncpy(option4,line,50);
 
 			if (i >1&&i<=7)
 			{
@@ -162,10 +149,10 @@ void edit_score(float , char []);
 
 			}
 			if (i == 8)
-			strncpy(correct,line,30);
+			strncpy(correct,line,50);
 
 			if (i == 9)
-			strncpy(category,line,30);
+			strncpy(category,line,50);
 		}
 
 
@@ -174,10 +161,8 @@ void edit_score(float , char []);
 	}
 
 	fclose(fp);
-
-	fclose(output);
-
-	output = fopen("Newtext.txt","r");
+    fclose(output);
+    output = fopen("Newtext.txt","r");
 
 	while(fgets(line,sizeof(line),output))
 	{
@@ -185,8 +170,9 @@ void edit_score(float , char []);
 	}
 
 	fclose(output);
-	    count=counter;
-	    if(counter>=3)
+	system("pause");
+    count=counter;
+    if(counter>=3)
    {  getch();
     fp = fopen("Quiz.txt","r");
    output = fopen("NewText.txt","w");
@@ -195,14 +181,23 @@ void edit_score(float , char []);
 	{
 	system("cls");
 	printf("\n\nSORRY YOU ARE NOT ELIGIBLE TO PLAY THIS GAME, BETTER LUCK NEXT TIME");
-	getch();
-	goto mainhome;
+	system("pause");
+	exit(1);
 	}
      test:
      system("cls");
      printf("\n\n\t*** CONGRATULATION %s you are eligible to play the Game ***",playername);
-     printf("\n\n\n\n\t!Press any key to Start the Game!");
-     getch();
+     printf("\n\n\n\n\t!Press N to go next level of the Game!");
+    if (toupper(getch())=='N'){
+            goto M;
+            system("cls");
+    }
+    else
+		{
+        goto mainhome;
+       system("cls");
+       }
+    M:
      counter=0;
 	 k = 0;
 	 	for (i = 0; i < 30; i++)
@@ -237,16 +232,16 @@ void edit_score(float , char []);
 				}
 
 				if(i == 3)
-			strncpy(option1,line,30);
+			strncpy(option1,line,50);
 
 				if(i == 4)
-			strncpy(option2,line,30);
+			strncpy(option2,line,50);
 
 				if(i == 5)
-			strncpy(option3,line,30);
+			strncpy(option3,line,50);
 
 				if(i == 6)
-			strncpy(option4,line,30);
+			strncpy(option4,line,50);
 
 			if (i >1&&i<=7)
 			{
@@ -254,59 +249,157 @@ void edit_score(float , char []);
 
 			}
 			if (i == 8)
-			strncpy(correct,line,30);
+			strncpy(correct,line,50);
 
 			if (i == 9)
-			strncpy(category,line,30);
+			strncpy(category,line,50);
 		}
 
-
-	counter+=asked(option, option1, option2, option3, option4, correct, category,output,&score);
+    counter+=asked(option, option1, option2, option3, option4, correct, category,output,&score);
 		system("cls");
 	}
 	fclose(fp);
 
 	fclose(output);
+	 score=0.00;
+	score=(float)counter*10000;
+	edit_score(score,playername);
+	  output = fopen("Newtext.txt","r");
 
-	output = fopen("Newtext.txt","r");
-
-  	while(fgets(line,sizeof(line),output))
+	while(fgets(line,sizeof(line),output))
 	{
 		printf(" %s",line);
 	}
 
 	fclose(output);
-    // counter=counter-count;
-    score=0.00;
-	score=(float)counter*100000;
-	printf("%f",score);
-	if(score>0.00 && score<1000000)
-	{
-	   printf("\n\n\t\t**************** CONGRATULATION *****************");
-	     printf("\n\t You won $%.2f",score);goto go;}
-     else if(score==1000000.00)
-	{
-	    printf("\n\n\n \t\t**************** CONGRATULATION ****************");
-	    printf("\n\t\t\t\t YOU ARE A MILLIONAIRE!!!!!!!!!");
-	    printf("\n\t\t You won $%.2f",score);
-	    printf("\t\t Thank You!!");
-	}
-	 else
-    {
-	 printf("\n\n\t******** SORRY YOU DIDN'T WIN ANY CASH ********");
-	    printf("\n\t\t Thanks for your participation");
-	    printf("\n\t\t TRY AGAIN");goto go;}
 
-	go:
-	    remove("Newtext.txt");
-	puts("\n\n Press Y if you want to play next game");
-	puts(" Press any key if you want to go main menu");
-	if (toupper(getch())=='Y')
-		goto home;
-	else
-		{
-		edit_score(score,playername);
-		goto mainhome;}
-    }
+	system("pause");
+	printf("\n\n\n\n\n");
+	printf("\n\n\n\t\t\t\t\t WELL PLAYED %s",playername);
+    printf("\n\n\t\t\t **************** CONGRATULATION!!!*****************");
+     printf("\n\n\t\t\t\tYou won $%.2f",score);
+     remove("Newtext.txt");
+     	puts("\n\n Thankyou for playing the game");
+	puts("\n Press any key if you want to exit from the game");
+	getch();
+   }
         return 0;
+    }
+    void show_record()
+    {system("cls");
+	char name[20];
+	float scr;
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&name,&scr);
+	printf("\n\n\t\t*************************************************************");
+	printf("\n\n\t\t %s has secured the Highest Score %0.2f",name,scr);
+	printf("\n\n\t\t*************************************************************");
+	fclose(f);
+	getch();}
+void reset_score()
+    {system("cls");
+    float sc;
+	char nm[20];
+	FILE *f;
+	remove("score.txt");
+	f=fopen("score.txt","w");
+	  fclose(f);
+    }
+    void help()
+	{system("cls");
+    printf("\n\n                              HELP");
+    printf("\n -------------------------------------------------------------------------");
+    printf("\n ......................... C program Quiz Game...........");
+    printf("\n >> There are two rounds in the game, WARMUP ROUND & CHALLANGE ROUND");
+    printf("\n >> In WARMUP ROUND you will be asked a total of 5 questions to test your programming");
+    printf("\n    skills. You will be eligible to play the game if you can give atleast 3");
+    printf("\n    right answers otherwise you can't play the Game...........");
+    printf("\n >> Your game starts with the CHALLANGE ROUND. In this round you will be asked");
+    printf("\n    total 10 questions each right answer will be awarded $10,000.");
+    printf("\n    By this way you can win upto ONE Lakh cash prize in USD...............");
+    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
+    printf("\n    right option");
+    printf("\n >> You will be asked questions continuously if you keep giving the right answers.");
+    printf("\n >> No negative marking for wrong answers");
+
+	printf("\n\n\t*********************BEST OF LUCK*********************************");
+	}
+		int asked(char option[size], const char option1[size], const char option2[size], const char option3[size], const char option4[size],
+	 const char correct[size], const char category[size],FILE * fp,float *counter)
+	{
+		printf("Your Option : ");
+		scanf("%str",&option[0]);
+		option[1] = '\0';
+		strupr(option);
+
+			char correctanswer[size];
+			char wronganswer[size];
+
+			fprintf(fp,"%s \n",category);
+
+		if (option[0] == correct[0])
+		{
+			if (option[0] == option1[0])
+				fprintf(fp," \nYou entered correct answer : %s  \n ", option1);
+			if (option[0] == option2[0])
+				fprintf(fp," \nYou entered correct answer : %s  \n ", option2);
+			if (option[0] == option3[0])
+				fprintf(fp," \nYou entered correct answer : %s  \n ", option3);
+			if (option[0] == option4[0])
+				fprintf(fp," \nYou entered correct answer : %s  \n ", option4);
+            fprintf(fp," -----------------------------------------------------------------------------\n");
+			fprintf(fp," -----------------------------------------------------------------------------\n");
+
+				counter = 1;
+				return counter;
+		}
+		else
+		{
+
+			if (correct[0] == option1[0])
+				strncpy(correctanswer, option1,24);
+			if (correct[0] == option2[0])
+				strncpy(correctanswer, option2,24);
+			if (correct[0] == option3[0])
+				strncpy(correctanswer, option3,24);
+			if (correct[0] == option4[0])
+				strncpy(correctanswer, option4,24);
+
+			correctanswer[24] = '\0';
+
+			if (option[0] == option1[0])
+				strncpy(wronganswer, option1,24);
+			if (option[0] == option2[0])
+				strncpy(wronganswer, option2,24);
+			if (option[0] == option3[0])
+				strncpy(wronganswer, option3,24);
+			if (option[0] == option4[0])
+				strncpy(wronganswer, option4,24);
+
+			wronganswer[24] = '\0';
+
+			fprintf(fp," You entered     : %s \n", wronganswer);
+			fprintf(fp," Correct answer  : %s \n", correctanswer);
+            fprintf(fp," -----------------------------------------------------------------------------\n");
+			fprintf(fp," -----------------------------------------------------------------------------\n");
+			return 0;
+		}
+
+	}
+void edit_score(float score, char plnm[20])
+	{
+	    system("cls");
+	float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&nm,&sc);
+	if (score>=sc)
+	  { sc=score;
+	    fclose(f);
+	    f=fopen("score.txt","w");
+	    fprintf(f,"%s\n%.2f",plnm,sc);
+fclose(f);
+	    }
     }
